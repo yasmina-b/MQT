8b4eb76921ec3db310290cf4e01581fb6969f8d4
yasmina-b <80706108+yasmina-b@users.noreply.github.com>
2023-10-23 22:48:25 +0300

    Laboratory 4

---
+++ MQT_Lab4/src/main/java/consumer/SimpleExampleConsumer.java
@@ -0,0 +1,65 @@
+package consumer;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.TopicPartition;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
+import java.time.temporal.ChronoUnit;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class SimpleExampleConsumer {
+    private static final Logger LOG = LoggerFactory.getLogger(SimpleExampleConsumer.class);
+
+    private static final String OUR_BOOTSTRAP_SERVERS = ":9092";
+    private static final String OFFSET_RESET = "earliest";
+    private static final String OUR_CONSUMER_GROUP_ID = "group_1";
+    private static final String topicName = "events2";
+    private static final String topicName2 = "events1";
+
+    KafkaConsumer<String, String> kafkaConsumer;
+
+    public SimpleExampleConsumer(Properties consumerPropsMap){
+        kafkaConsumer = new KafkaConsumer<String, String>(consumerPropsMap);
+    }
+
+    public static Properties buildConsumerPropsMap(){
+        Properties props = new Properties();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, OUR_BOOTSTRAP_SERVERS);
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, OUR_CONSUMER_GROUP_ID);
+        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, OFFSET_RESET);
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+
+        return props;
+    }
+
+    public void pollKafka(String kafkaTopicName){
+
+        kafkaConsumer.subscribe(Collections.singleton(kafkaTopicName));
+        //kafkaConsumer.subscribe(List.of(topicName, topicName2));
+
+        Duration pollingTime = Duration.of(2, ChronoUnit.SECONDS);
+        while (true){
+            // get records from kafka
+            //The poll method is a blocking method waiting for specified time in seconds.
+            // If no records are available after the time period specified, the poll method returns an empty ConsumerRecords.
+            ConsumerRecords<String, String> records = kafkaConsumer.poll(pollingTime);
+
+            // consume the records
+            records.forEach(crtRecord -> {
+                LOG.info("------ Simple Example Consumer ------------- topic ={}  key = {}, value = {} => partition = {}, offset = {}",kafkaTopicName, crtRecord.key(), crtRecord.value(), crtRecord.partition(), crtRecord.offset());
+            });
+        }
+    }
+
+    public static void main(String[] args) {
+        SimpleExampleConsumer consumer = new SimpleExampleConsumer(buildConsumerPropsMap());
+        consumer.pollKafka("events2");
+    }
+}
